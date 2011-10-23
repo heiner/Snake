97f7ceae4a5ed48fc2865ae87bdefe1aa0fa8433
Heinrich Kuettler <heinrich.kuettler@gmail.com>
2011-10-23 16:23:27 +0200

    Commit new Android project to Git

---
+++ src/com/kuettler/snake/SnakeActivity.java
@@ -0,0 +1,15 @@
+package com.kuettler.snake;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SnakeActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
